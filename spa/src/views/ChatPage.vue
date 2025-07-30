a53c40c13b653b0179dc187861eaba0ae4822c98
MouJieQin <724174637@qq.com>
2025-07-30 23:07:19 +0800

    Enlarge the waiting time for English in auto plyaing

--- spa/src/views/ChatPage.vue
+++ spa/src/views/ChatPage.vue
@@ -239,7 +239,7 @@ const handleResponseClosed = (data: any, result: ProcessResult) => {
         webSocket?.value?.sendGenerateAudioFiles(data.message_id, 0, 0)
         setTimeout(() => {
             webSocket?.value?.sendPlayMessage(data.message_id);
-        }, 1000)
+        }, sessionAiConfig.value.language === 'English' ? 3000 : 1000)
     }
 }
 
